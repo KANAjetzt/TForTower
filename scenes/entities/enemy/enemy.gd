4bd69319049146d18a3f948868ff49121a505fdf
Kai Roth <kai@kana.jetzt>
2023-02-10 21:11:59 +0100

    ✔ enemy movement base

---
+++ scenes/entities/enemy/enemy.gd
@@ -0,0 +1,20 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	move_and_slide()
+
+func _ready():
+	initialize(position, Vector3.ZERO)
+
+func initialize(start_position, player_position) -> void:
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	
+	velocity = Vector3.FORWARD * 1.0
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
