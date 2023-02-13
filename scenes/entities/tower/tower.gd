6f06f7a51c4c2e34947f0f62d07e362f80804875
Kai Roth <kai@kana.jetzt>
2023-02-13 11:11:30 +0100

    🔨 fixed tower only starts shooting after first targeted enemy is removed

--- scenes/entities/tower/tower.gd
+++ scenes/entities/tower/tower.gd
@@ -4,13 +4,20 @@ signal closest_changed(body)
 
 @onready var range = $Range
 @onready var weapons = $Weapons
+@onready var label = $CanvasLayer/Label
+
 
 @onready var body_closest = null
 
 func _ready():
-	pass
+	get_closest()
+
 
 func _physics_process(delta):
+	get_closest()
+
+
+func get_closest() -> void:
 	# Update the position of the current cloests body
 	if body_closest != null && is_instance_valid(body_closest.body):
 		body_closest.distance = global_position.distance_squared_to(body_closest.body.position)
@@ -23,12 +30,14 @@ func _physics_process(delta):
 		# Get the distance from the tower to the current enemy
 		var distance_current := global_position.distance_squared_to(body.position)
 		
-		# If there is no closest body yet use this one
+		# If there is no closest body yet use this one and send closest_changed signal.
 		if body_closest == null || !is_instance_valid(body_closest.body):
 			body_closest = {
 				"body": body,
 				"distance": distance_current
 			}
+			
+			emit_signal("closest_changed", body_closest.body)
 		
 		# If the distance to current closest enemy is higher then to the current one.
 		# Update the closest enemy and send closest_changed signal.
